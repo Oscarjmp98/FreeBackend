b35a1c0137bbaf2ae404231231e110eba37887cd
Oscar Medina Paz <oscar.medina01@unicatolica.edu.co>
2024-11-29 18:24:08 -0500

    Cambios en el BackEnd

--- routes/signos.routes.js
+++ routes/signos.routes.js
@@ -7,18 +7,12 @@ const multer = require('multer');
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 router
-    // .get('/', signoController.getAllSignos)
-    // .get('/:categoriaU/:signoU', signoController.getOneSigno)
-
-    .post('/codigo', signoController.registarCodigo)
+   
     .post('/registro', signoController.registroCredenciales)
     .post('/login',signoController.validarCredenciales)
     .post('/registroadmin',signoController.registarAdmin)
-    .get('/traer/:valor',signoController.ganadores)
-    .get('/traerusuario/:iduser',signoController.renderizar)
-    .post('/generar',signoController.generarCodigo)
     .post('/upload/:iduser', upload.single('video'), signoController.Subirvideo)
     .get('/videos/:iduser',signoController.videos)
-    // .patch('/restablecer', signoController.editarContrasena)
+    
 
 module.exports = router;
